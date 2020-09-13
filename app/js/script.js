c3a415359172663adf63789bd30e2ce3b298e6e7
Matheus <matheus1250.mc@gmail.com>
2020-09-13 11:44:36 -0300

    overlay animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,19 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", () => {
 	console.log("click hamburger");
 
-	header.classList.toggle("open");
+	if (header.classList.contains("open")) {
+		// close menu
+		header.classList.remove("open");
+		overlay.classList.remove("fade-in");
+		overlay.classList.add("fade-out");
+	} else {
+		// open menu
+		header.classList.add("open");
+		overlay.classList.remove("fade-out");
+		overlay.classList.add("fade-in");
+	}
 });
